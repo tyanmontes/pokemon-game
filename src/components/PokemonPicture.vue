bc3a1be3f062a448d85f766e972e805e4d18c98c
Tyan <sebastians2896@gmail.com>
2024-01-15 18:11:51 -0500

    pokemon-game

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="pokemon-container">
+        
+        <img :src="imgSrc"
+             class="hidden-pokemon"
+             alt="">
+        <img v-if="showPokemon" :src="imgSrc" 
+             class="fade-in"
+             alt="">
+    </div>
+</template>
+<script>
+export default{
+    props:{
+        pokemonId:{
+            type: Number,
+            required: true
+        },
+        showPokemon:{
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    },
+    computed: {
+        imgSrc(){
+            return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ this.pokemonId }.svg`
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* Pokemon Picture */
+.pokemon-container {
+    height: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+img {
+    height: 200px;
+    position: absolute;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+
+.hidden-pokemon {
+    filter: brightness(0);
+}
+
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,34 @@
+import PokemonPicture from '/components/PokemonPicture';
+<template>
+    <h1>¿Quién es este pokémon</h1>
+    <PokemonPicture  :pokemonId="40" :showPokemon="true"/>
+    <PokemonOptions  :pokemons="pokemonArr"/>
+</template>
+<script>
+  import PokemonPicture from '@/components/PokemonPicture'
+  import PokemonOptions from '@/components/PokemonOptions';
+  import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+export default {
+    name: 'App',
+    components: {
+      PokemonPicture,
+      PokemonOptions
+    },
+    data() {
+        return{
+
+           pokemonArr: []
+
+        }
+    },
+    methods:{
+       async mixPokemonArray(){
+          this.pokemonArr = await getPokemonOptions()
+       }
+    },
+    mounted(){
+       this.mixPokemonArray()
+    }
+  }
+  </script>
